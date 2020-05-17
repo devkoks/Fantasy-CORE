3eb2ff6e02f45f8248a6cce5fd9ed960cca7eb0e
{Koks} <koks@blue-creature.com>
2020-05-18 01:36:20 +0600

    Fixed bug

--- app/tplEngine.php
+++ app/tplEngine.php
@@ -1,10 +1,4 @@
 <?php
 $view = $this->core->modules["tpl"]->comments('{/*','*/}',$view);
 
-$view = $this->core->modules["tpl"]->removeTag('active:module:'.$this->getModule(),$view);
-$view = $this->core->modules["tpl"]->hidden('active:module:(.*?)',$view);
-
-$view = $this->core->modules["tpl"]->hidden('unactive:module:'.$this->getModule(),$view);
-$view = $this->core->modules["tpl"]->removeTag('unactive:module:(.*?)',$view);
-
 $view = str_replace('{module}',$this->getModule(),$view);
